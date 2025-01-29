1cba3929f5252d34056762183dd7020b3377f925
PradeepaRa <prathee.provab@gmail.com>
2025-01-30 04:56:29 +0530

    Update thursday index.php

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
-echo "Hello World ! This is my first file in testing CICD jenkins pipeline process.First CICD test started .id_rsa connection testttttttttttttt";
+echo "Hello World ! This is my first file in testing CICD jenkins pipeline process.First CICD test started .id_rsa connection testttttttttttttt check";
 ?>
