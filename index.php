a54398e611b55d8900161370c65c353e72e34153
PradeepaRa <prathee.provab@gmail.com>
2025-02-05 07:24:30 +0530

    Update index.php
    ssh commit

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
-echo "Hello World ! This is my first file in testing CICD jenkins pipeline process.First CICD test started .id_rsa connection testttttttttttttt check";
+echo "Hello World ! This is my first file after ssh check";
 ?>
